45688547b0111fa7ff1aa2ddd770b12c7295095a
gerardoafa <gerardo_fuentes@unitec.edu>
2026-02-14 21:52:48 -0600

    Proyecto entero

--- ClaseSemana4.API/Program.cs
+++ ClaseSemana4.API/Program.cs
@@ -1,11 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blockbuster.API.Services;
+using Microsoft.OpenApi;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<FirebaseService>();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MiPrimeraApi",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +23,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "MiPrimeraApi v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
